6bc874895a121074d9eea9c5647f40d15365b928
shahinul islam <shahinul37@outlook.com>
2024-05-30 20:24:33 +0600

    first commit for drum kit 01

--- DrumKit-01/main.js
+++ DrumKit-01/main.js
@@ -1,9 +1,35 @@
+const keyContainer = document.querySelector(".key-container");
+
+fetch("./assets/data.json")
+	.then((res) => res.json())
+	.then((data) => {
+		data.forEach((key) => {
+			const keyPad = document.createElement("div");
+			keyPad.classList.add("key-pad");
+			keyPad.setAttribute("id", key.key);
+			keyPad.innerHTML = `${key.key} <span>${key.name}</span>`;
+			keyContainer.appendChild(keyPad);
+		});
+	});
+
+let currentAudio = null;
+
 document.addEventListener("keydown", function (event) {
-	let key = event.key;
-	console.log(event);
-	document.getElementById(`${key}`).classList.add("key-pad-effect");
-	setTimeout(function () {
-		document.getElementById(`${key}`).classList.remove("key-pad-effect");
-	}, 100);
-	new Audio(`/assets/audio/${key}.mp3`).play();
+	let key = event.key.toUpperCase();
+	let keyPad = document.getElementById(key);
+
+	if (keyPad) {
+		if (currentAudio) {
+			currentAudio.pause();
+			currentAudio.currentTime = 0;
+		}
+
+		keyPad.classList.add("key-pad-effect");
+		currentAudio = new Audio(`./assets/audio/${key}.mp3`);
+		currentAudio.play();
+
+		setTimeout(function () {
+			keyPad.classList.remove("key-pad-effect");
+		}, 100);
+	}
 });
